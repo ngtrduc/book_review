026a4472e1587dd394e01885b863c31eeaeb4fed
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-10-09 15:00:19 +0700

    admin home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "users/registrations",
-      omniauth_callbacks: "users/omniauth_callbacks"}
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
 
@@ -8,6 +7,10 @@ Rails.application.routes.draw do
   get  "static_pages/help"
   get  "static_pages/about"
   get  "static_pages/contact"
+
+  namespace :admin do
+    root "dashboard#index"
+  end
 
   resources :users, only: [:edit, :update]
 
