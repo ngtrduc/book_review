77942c1253ac726ba80dccd6bcec5f443bdb7865
Hai Nguyen <mr.haicaca@gmail.com>
2016-10-13 16:15:20 +0700

    google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   enum role: [:admin, :user]
 
@@ -16,6 +16,26 @@ class User < ActiveRecord::Base
     foreign_key: :followed_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  def self.find_for_google_oauth2 access_token, signed_in_resource = nil
+    data = access_token.info
+    user = User.where(provider: access_token.provider, uid: access_token.uid ).first
+    unless user
+      registered_user = User.where(email: access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(
+          name: data["name"],
+          provider: access_token.provider,
+          email: data["email"],
+          uid: access_token.uid ,
+          password: Devise.friendly_token[0,20],
+        )
+      end
+    end
+    user
+  end
 
   def is_user? user
     self == user
