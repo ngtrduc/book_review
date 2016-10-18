fc1ace4d18b3ccf301d17925de13d95ac2e2f43c
tohai <tohaia2k47@gmail.com>
2016-10-18 09:37:41 +0000

    fix err last weeks

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,25 @@
 
 ActiveRecord::Schema.define(version: 20160705071250) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
-    t.integer  "action_type"
-    t.integer  "other_id"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "activities", ["user_id"], name: "index_activities_on_user_id"
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -36,7 +46,7 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.datetime "updated_at",   null: false
   end
 
-  add_index "books", ["category_id"], name: "index_books_on_category_id"
+  add_index "books", ["category_id"], name: "index_books_on_category_id", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -53,8 +63,8 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["review_id"], name: "index_comments_on_review_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["review_id"], name: "index_comments_on_review_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id"
@@ -63,8 +73,8 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "favorites", ["book_id"], name: "index_favorites_on_book_id"
-  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
+  add_index "favorites", ["book_id"], name: "index_favorites_on_book_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "likes", force: :cascade do |t|
     t.integer  "status"
@@ -74,8 +84,8 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "likes", ["activity_id"], name: "index_likes_on_activity_id"
-  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
+  add_index "likes", ["activity_id"], name: "index_likes_on_activity_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "marks", force: :cascade do |t|
     t.integer  "status"
@@ -86,8 +96,8 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "marks", ["book_id"], name: "index_marks_on_book_id"
-  add_index "marks", ["user_id"], name: "index_marks_on_user_id"
+  add_index "marks", ["book_id"], name: "index_marks_on_book_id", using: :btree
+  add_index "marks", ["user_id"], name: "index_marks_on_user_id", using: :btree
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -96,15 +106,19 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.datetime "updated_at",  null: false
   end
 
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
+
   create_table "requests", force: :cascade do |t|
     t.string   "content"
-    t.integer  "status"
+    t.integer  "status",     default: 0
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "requests", ["user_id"], name: "index_requests_on_user_id"
+  add_index "requests", ["user_id"], name: "index_requests_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.string   "content"
@@ -115,8 +129,8 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "reviews", ["book_id"], name: "index_reviews_on_book_id"
-  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
+  add_index "reviews", ["book_id"], name: "index_reviews_on_book_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -136,7 +150,19 @@ ActiveRecord::Schema.define(version: 20160705071250) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "books", "categories"
+  add_foreign_key "comments", "reviews"
+  add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "books"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "likes", "activities"
+  add_foreign_key "likes", "users"
+  add_foreign_key "marks", "books"
+  add_foreign_key "marks", "users"
+  add_foreign_key "requests", "users"
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
