7392d5227601873a276aa93a64d1fff1e4b7206e
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-11-15 11:43:42 +0700

    user-page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,3 +54,8 @@ function image_preview() {
 }
 $(document).ready(image_preview);
 $(document).on('page:load', image_preview);
+
+function textAreaAdjust(o) {
+  o.style.height = "1px";
+  o.style.height = (25+o.scrollHeight)+"px";
+}
